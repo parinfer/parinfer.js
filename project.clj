86b19d72f0893b51360d87eb855725a37b1937cf
Shaun Williams <shaun.e.williams@gmail.com>
2015-09-22 01:35:34 -0500

    start simple delim parser (having problems controlling textarea value for preventing delim imbalance)

---
+++ project.clj
@@ -0,0 +1,68 @@
+(defproject parinfer "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.122"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [sablono "0.3.6"]
+                 [org.omcljs/om "0.9.0"]]
+
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.4.0"]]
+
+  :source-paths ["src"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src"]
+
+              :figwheel { :on-jsload "parinfer.core/on-js-reload" }
+
+              :compiler {:main parinfer.core
+                         :asset-path "js/compiled/out"
+                         :output-to "resources/public/js/compiled/parinfer.js"
+                         :output-dir "resources/public/js/compiled/out"
+                         :source-map-timestamp true }}
+             {:id "min"
+              :source-paths ["src"]
+              :compiler {:output-to "resources/public/js/compiled/parinfer.js"
+                         :main parinfer.core
+                         :optimizations :advanced
+                         :pretty-print false}}]}
+
+  :figwheel {
+             ;; :http-server-root "public" ;; default and assumes "resources" 
+             ;; :server-port 3449 ;; default
+             ;; :server-ip "127.0.0.1" 
+
+             :css-dirs ["resources/public/css"] ;; watch and update CSS
+
+             ;; Start an nREPL server into the running figwheel process
+             ;; :nrepl-port 7888
+
+             ;; Server Ring Handler (optional)
+             ;; if you want to embed a ring handler into the figwheel http-kit
+             ;; server, this is for simple ring servers, if this
+             ;; doesn't work for you just run your own server :)
+             ;; :ring-handler hello_world.server/handler
+
+             ;; To be able to open files in your editor from the heads up display
+             ;; you will need to put a script on your path.
+             ;; that script will have to take a file path and a line number
+             ;; ie. in  ~/bin/myfile-opener
+             ;; #! /bin/sh
+             ;; emacsclient -n +$2 $1
+             ;;
+             ;; :open-file-command "myfile-opener"
+
+             ;; if you want to disable the REPL
+             ;; :repl false
+
+             ;; to configure a different figwheel logfile path
+             ;; :server-logfile "tmp/logs/figwheel-logfile.log" 
+             })
---
+++ src/parinfer/core.cljs
@@ -0,0 +1,166 @@
+(ns ^:figwheel-always parinfer.core
+  (:require
+    [clojure.string :as string :refer [split-lines]]
+    [om.core :as om :include-macros true]
+    [om.dom :as dom :include-macros true]
+    [sablono.core :refer-macros [html]]))
+
+(enable-console-print!)
+
+(defonce app-state (atom {:text ""}))
+
+(defn remove-trailing-closers
+  [line]
+  (string/replace line #"[\]})]+\s*$" ""))
+
+;;------------------------------------------------------------------------
+;; Simple clojure delimiter parsing
+;;------------------------------------------------------------------------
+
+(def matching-delim
+  {"{" "}", "}" "{"
+   "[" "]", "]" "["
+   "(" ")", ")" "("})
+
+(def char-hierarchy
+  (-> (make-hierarchy)
+      (derive "{" :open)
+      (derive "[" :open)
+      (derive "(" :open)
+      (derive "}" :close)
+      (derive "]" :close)
+      (derive ")" :close)))
+
+;; Simple state tracking by checking last character.
+
+(defn prev-ch [stack]
+  (second (peek stack)))
+
+(defn in-escape? [stack]
+  (= \\ (prev-ch stack)))
+
+(defn in-str? [stack]
+  (= \" (prev-ch stack)))
+
+(defn in-comment? [stack]
+  (= \; (prev-ch stack)))
+
+(defn in-code? [stack]
+  (and (not (in-str? stack))
+       (not (in-comment? stack))))
+
+(defn valid-closer? [stack ch]
+  (= (prev-ch stack) (matching-delim ch)))
+
+;; Push operations
+
+(defmulti push-char
+  (fn [stack [i ch]] ch)
+  :hierarchy #'char-hierarchy)
+
+(defmethod push-char :open
+  [stack [i ch]]
+  (cond
+    (in-escape? stack) (pop stack)
+    (in-code? stack) (conj stack [i ch])
+    :else stack))
+
+(defmethod push-char :close
+  [stack [i ch]]
+  (println (valid-closer? stack ch) (in-code? stack))
+  (cond
+    (in-escape? stack) (pop stack)
+    (in-code? stack) (if (valid-closer? stack ch)
+                       (pop stack)
+                       (do
+                         (swap! app-state assoc :stack stack)
+                         (throw (str "no matching open delimiter for " ch))))
+    :else stack))
+
+(defmethod push-char ";"
+  [stack [i ch]]
+  (cond
+    (in-escape? stack) (pop stack)
+    (in-code? stack) (conj stack [i ch])
+    :else stack))
+
+(defmethod push-char "\n"
+  [stack [i ch]]
+  (let [s0 (cond-> stack (in-escape? stack) pop)
+        s1 (cond-> s0 (in-comment? s0) pop)]
+    s1))
+
+(defmethod push-char "\\"
+  [stack [i ch]]
+  (cond
+    (in-escape? stack) (pop stack)
+    :else (conj stack [i ch])))
+
+(defmethod push-char "\""
+  [stack [i ch]]
+  (cond
+    (in-escape? stack) (pop stack)
+    (in-str? stack) (pop stack)
+    :else (conj stack [i ch])))
+
+(defmethod push-char :default
+  [stack [i ch]]
+  (cond
+    (in-escape? stack) (pop stack)
+    :else stack))
+
+;; main parsers
+
+(defn parse-line
+  ([line] (parse-line [] line))
+  ([stack line] (reduce push-char stack (map-indexed vector (str line "\n")))))
+
+(defn parse-text
+  ([text] (parse-text [] text))
+  ([stack text] (reduce parse-line stack (split-lines text))))
+
+
+(defn add-trailing-closers
+  [lines]
+  )
+
+;; EVENTS
+;; when editing current line, prevent invalid closers
+;; when leaving current line, remove trailing closers
+;; when edited, updated paren overlay
+
+;; VIEW
+
+(defn update-text!
+  [cursor-index text]
+  (println cursor-index)
+  (try
+    (let [stack (parse-text text)]
+      (swap! app-state assoc :stack stack)
+      (swap! app-state assoc :text text))
+    (catch :default e
+      nil ;; don't update the text
+      )))
+
+(defn root-comp
+  [data owner]
+  (reify
+    om/IRender
+    (render [_this]
+      (html
+        [:div
+         [:textarea
+          {:ref "txt"
+           :on-change (fn [evt]
+                        (let [target (.-target evt)]
+                          (update-text! (.-selectionStart target) (.-value target))))
+           :value (:text data)}]
+         [:ul
+          (for [[i ch] (:stack data)]
+            [:li i " " ch])]]))))
+
+(om/root
+  root-comp
+  app-state
+  {:target (. js/document (getElementById "app"))})
+
