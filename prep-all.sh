2703a44e1fdb642f10c7d569e850f74bd714efeb
Shaun Williams <shaun.e.williams@gmail.com>
2015-11-10 15:23:22 -0600

    add usage string to prep-all script

--- prep-all.sh
+++ prep-all.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+if [ "$#" -ne 1 ]; then
+  echo
+  echo "prep-all.sh:  correct indentation of all clojure files in the given directory (recursive)"
+  echo "  (originals backed up with .bak files)"
+  echo
+  exit 1
+fi
+
 find $1 -type f -name "*.clj" -or -name "*.clj?" | xargs lein prep
