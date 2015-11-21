fb8997de9a1e9ffe96965eb1c24df4c8874f581d
Shaun Williams <shaun.e.williams@gmail.com>
2015-11-21 00:34:37 -0600

    zero-out unused version number in parinfer-site project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject parinfer-site "0.1.0-SNAPSHOT"
+(defproject parinfer-site "0.0-SNAPSHOT"
   :description "site for illustrating the value of Parinfer"
   :url "http://shaunlebron.github.io/parinfer"
 
