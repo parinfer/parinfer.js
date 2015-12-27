1ca5b3d8f9663af493a1287d9db40d9f02af972b
Shaun Williams <shaun.e.williams@gmail.com>
2015-12-27 07:19:22 -0600

    start parinfer.cpp for asm.js (#74)

---
+++ lib/parinfer.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+//------------------------------------------------------------------------------
+// Constants
+//------------------------------------------------------------------------------
+
+string BACKSLASH = "\\",
+       COMMA = ",",
+       DOUBLE_QUOTE = "\"",
+       NEWLINE = "\n",
+       SEMICOLON = ";",
+       TAB = "\t";
+
+map<string, string> PARENS {
+  {"{", "}"},
+  {"}", "{"},
+  {"[", "]"},
+  {"]", "["},
+  {"(", ")"},
+  {")", "("}
+};
+
+//------------------------------------------------------------------------------
+// Result Structure
+//------------------------------------------------------------------------------
+
+struct insert_t {
+  bool present = false;
+  int lineNo;
+  int x;
+};
+
+struct range_t {
+  bool present = false;
+  int start;
+  int end;
+};
+
+struct opener_t {
+  string ch;
+  int x;
+  int indentDelta;
+};
+
+struct cursor_t {
+  bool present = false;
+  int lineNo;
+  int x;
+  int dx;
+};
+
+struct indent_t {
+  bool present = false;
+  int value;
+};
+
+typedef vector<opener_t> opener_stack_t;
+
+struct result_t {
+  vector<string> lines;
+  int lineNo = 0;
+  string ch = "";
+  int x = 0;
+  opener_stack_t stack;
+  opener_stack_t backup;
+  insert_t insert;
+  range_t parenTrail;
+  cursor_t cursor;
+  bool quoteDanger = false;
+  bool trackIndent = false;
+  bool cursorInComment = false;
+  bool quit = false;
+  bool process = false;
+  bool success = false;
+  indent_t maxIndent;
+  int indentDelta = 0;
+} result;
+
+//------------------------------------------------------------------------------
+// String Operations
+//------------------------------------------------------------------------------
+
+// insertString ->       std::string.insert
+// replaceStringRange -> std::string.replace
+// removeStringRange ->  std::string.erase
+
+//------------------------------------------------------------------------------
+// Reader Operations
+//------------------------------------------------------------------------------
+
+bool isOpenParen(const string& c) {
+  return c == "{" || c == "(" || c == "[";
+}
+
+bool isCloseParen(const string& c) {
+  return c == "}" || c == ")" || c == "]";
+}
+
+bool isWhitespace(const string& c) {
+  return c == " " || c == TAB || c == NEWLINE;
+}
+
+//------------------------------------------------------------------------------
+// Lisp Reader: Stack States
+//------------------------------------------------------------------------------
+
+bool isEscaping(const opener_stack_t& stack) {
+  if (stack.size() > 0) {
+    return stack.back().ch == BACKSLASH;
+  }
+  return false;
+}
+
+bool isPrevNonEscCh(const opener_stack_t& stack, const string& ch) {
+  int i = stack.size() - (isEscaping(stack) ? 2 : 1);
+  if (i >= 0) {
+    return stack[i].ch == ch;
+  }
+  return false;
+}
+
+bool isInStr(const opener_stack_t& stack) {
+  return isPrevNonEscCh(stack, DOUBLE_QUOTE);
+}
+
+bool isInComment(const opener_stack_t& stack) {
+  return isPrevNonEscCh(stack, SEMICOLON);
+}
+
+bool isInCode(const opener_stack_t& stack) {
+  return !isInStr(stack) && !isInComment(stack);
+}
+
+bool isValidCloser(const opener_stack_t& stack, const string& ch) {
+  int i = stack.size() - 1;
+  if (i >= 0) {
+    return stack[i].ch == ch;
+  }
+  return false;
+}
+
+//------------------------------------------------------------------------------
+// Main
+//------------------------------------------------------------------------------
+
+int main() {
+  return 0;
+}
