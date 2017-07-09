f4be24820ebe108f1af9bcde986c103d0c031721
Shaun Williams <shaun.e.williams@gmail.com>
2017-07-09 01:39:35 -0500

    update deploy

--- parinfer.js
+++ parinfer.js
@@ -1,5 +1,5 @@
 //
-// Parinfer 2.5.0
+// Parinfer 2.5.1
 //
 // Copyright 2015-2016 © Shaun LeBron
 // MIT License
@@ -33,11 +33,10 @@
 //------------------------------------------------------------------------------
 
 // NOTE: this is a performance hack
-// The main result object uses a lot of "Integer or null" values.
-// Using a sentinel integer is faster than actual null because it cuts down on
+// The main result object uses a lot of "unsigned integer or null" values.
+// Using a negative integer is faster than actual null because it cuts down on
 // type coercion overhead.
-// https://en.wikipedia.org/wiki/Sentinel_value
-var SENTINEL_NULL = -999;
+var UINT_NULL = -999;
 
 var INDENT_MODE = "INDENT_MODE",
     PAREN_MODE = "PAREN_MODE";
@@ -109,8 +108,8 @@ function getInitialResult(text, options, mode) {
     mode: mode,                // [enum] - current processing mode (INDENT_MODE or PAREN_MODE)
 
     origText: text,            // [string] - original text
-    origCursorX: SENTINEL_NULL,
-    origCursorLine: SENTINEL_NULL,
+    origCursorX: UINT_NULL,
+    origCursorLine: UINT_NULL,
 
     inputLines:                // [string array] - input lines that we process line-by-line, char-by-char
       text.split(LINE_ENDING_REGEX),
@@ -130,21 +129,21 @@ function getInitialResult(text, options, mode) {
                                // keys {ch, x, lineNo}, which is just the state of the `parenStack` at the cursor line.
 
     parenTrail: {              // the range of parens at the end of a line
-      lineNo: SENTINEL_NULL,   // [integer] - line number of the last parsed paren trail
-      startX: SENTINEL_NULL,   // [integer] - x position of first paren in this range
-      endX: SENTINEL_NULL,     // [integer] - x position after the last paren in this range
+      lineNo: UINT_NULL,       // [integer] - line number of the last parsed paren trail
+      startX: UINT_NULL,       // [integer] - x position of first paren in this range
+      endX: UINT_NULL,         // [integer] - x position after the last paren in this range
       openers: []              // [array of stack elements] - corresponding open-paren for each close-paren in this range
     },
 
-    cursorX: SENTINEL_NULL,    // [integer] - x position of the cursor
-    cursorLine: SENTINEL_NULL, // [integer] - line number of the cursor
-    cursorDx: SENTINEL_NULL,   // [integer] - amount that the cursor moved horizontally if something was inserted or deleted
+    cursorX: UINT_NULL,        // [integer] - x position of the cursor
+    cursorLine: UINT_NULL,     // [integer] - line number of the cursor
+    cursorDx: UINT_NULL,       // [integer] - amount that the cursor moved horizontally if something was inserted or deleted
 
     isInCode: true,            // [boolean] - indicates if we are currently in "code space" (not string or comment)
     isEscaping: false,         // [boolean] - indicates if the next character will be escaped (e.g. `\c`).  This may be inside string, comment, or code.
     isInStr: false,            // [boolean] - indicates if we are currently inside a string
     isInComment: false,        // [boolean] - indicates if we are currently inside a comment
-    commentX: SENTINEL_NULL,   // [integer] - x position of the start of comment on current line (if any)
+    commentX: UINT_NULL,       // [integer] - x position of the start of comment on current line (if any)
 
     quoteDanger: false,        // [boolean] - indicates if quotes are imbalanced inside of a comment (dangerous)
     trackingIndent: false,     // [boolean] - are we looking for the indentation point of the current line?
@@ -153,19 +152,19 @@ function getInitialResult(text, options, mode) {
     partialResult: false,      // [boolean] - should we return a partial result when an error occurs?
     forceBalance: false,       // [boolean] - should indent mode aggressively enforce paren balance?
 
-    maxIndent: SENTINEL_NULL,  // [integer] - maximum allowed indentation of subsequent lines in Paren Mode
+    maxIndent: UINT_NULL,      // [integer] - maximum allowed indentation of subsequent lines in Paren Mode
     indentDelta: 0,            // [integer] - how far indentation was shifted by Paren Mode
                                //  (preserves relative indentation of nested expressions)
 
     error: {                   // if 'success' is false, return this error to the user
-      name: SENTINEL_NULL,     // [string] - Parinfer's unique name for this error
-      message: SENTINEL_NULL,  // [string] - error message to display
-      lineNo: SENTINEL_NULL,   // [integer] - line number of error
-      x: SENTINEL_NULL,        // [integer] - start x position of error
+      name: null,              // [string] - Parinfer's unique name for this error
+      message: null,           // [string] - error message to display
+      lineNo: null,            // [integer] - line number of error
+      x: null,                 // [integer] - start x position of error
       extra: {
-        name: SENTINEL_NULL,
-        lineNo: SENTINEL_NULL,
-        x: SENTINEL_NULL
+        name: null,
+        lineNo: null,
+        x: null
       }
     },
     errorPosCache: {}          // [object] - maps error name to a potential error position
@@ -236,12 +235,12 @@ function error(result, name) {
     lineNo: cache ? cache[keyLineNo] : result[keyLineNo],
     x: cache ? cache[keyX] : result[keyX]
   };
-  var opener = peek(result.parenStack);
+  var opener = peek(result.parenStack, 1);
 
   if (name === ERROR_UNMATCHED_CLOSE_PAREN) {
     // extra error info for locating the open-paren that it should've matched
     cache = result.errorPosCache[ERROR_UNMATCHED_OPEN_PAREN];
-    if (cache || opener !== SENTINEL_NULL) {
+    if (cache || opener) {
       e.extra = {
         name: ERROR_UNMATCHED_OPEN_PAREN,
         lineNo: cache ? cache[keyLineNo] : opener[keyLineNo],
@@ -306,7 +305,7 @@ function shiftCursorOnEdit(result, lineNo, start, end, replace) {
 
   if (dx !== 0 &&
       result.cursorLine === lineNo &&
-      result.cursorX !== SENTINEL_NULL &&
+      result.cursorX !== UINT_NULL &&
       isCursorAffected(result, start, end)) {
     result.cursorX += dx;
   }
@@ -330,7 +329,7 @@ function initLine(result, line) {
   result.lines.push(line);
 
   // reset line-specific state
-  result.commentX = SENTINEL_NULL;
+  result.commentX = UINT_NULL;
   result.indentDelta = 0;
   delete result.errorPosCache[ERROR_UNMATCHED_CLOSE_PAREN];
   delete result.errorPosCache[ERROR_UNMATCHED_OPEN_PAREN];
@@ -350,20 +349,17 @@ function commitChar(result, origCh) {
 //------------------------------------------------------------------------------
 
 function clamp(val, minN, maxN) {
-  if (minN !== SENTINEL_NULL) {
+  if (minN !== UINT_NULL) {
     val = Math.max(minN, val);
   }
-  if (maxN !== SENTINEL_NULL) {
+  if (maxN !== UINT_NULL) {
     val = Math.min(maxN, val);
   }
   return val;
 }
 
-function peek(array) {
-  if (array.length === 0) {
-    return SENTINEL_NULL;
-  }
-  return array[array.length - 1];
+function peek(array, i) {
+  return array[array.length - i];
 }
 
 //------------------------------------------------------------------------------
@@ -374,7 +370,7 @@ function isValidCloseParen(parenStack, ch) {
   if (parenStack.length === 0) {
     return false;
   }
-  return peek(parenStack).ch === MATCH_PAREN[ch];
+  return peek(parenStack, 1).ch === MATCH_PAREN[ch];
 }
 
 function onOpenParen(result) {
@@ -392,7 +388,7 @@ function onOpenParen(result) {
 }
 
 function onMatchedCloseParen(result) {
-  var opener = peek(result.parenStack);
+  var opener = peek(result.parenStack, 1);
   result.parenTrail.endX = result.x + 1;
   result.parenTrail.openers.push(opener);
   result.maxIndent = opener.x;
@@ -405,8 +401,8 @@ function onUnmatchedCloseParen(result) {
   }
   if (!result.errorPosCache[ERROR_UNMATCHED_CLOSE_PAREN]) {
     cacheErrorPos(result, ERROR_UNMATCHED_CLOSE_PAREN);
-    var opener = peek(result.parenStack);
-    if (opener !== SENTINEL_NULL) {
+    var opener = peek(result.parenStack, 1);
+    if (opener) {
       var e = cacheErrorPos(result, ERROR_UNMATCHED_OPEN_PAREN);
       e.inputLineNo = opener.inputLineNo;
       e.inputX = opener.inputX;
@@ -504,7 +500,7 @@ function onChar(result) {
 function isCursorOnLeft(result) {
   return (
     result.lineNo === result.cursorLine &&
-    result.cursorX !== SENTINEL_NULL &&
+    result.cursorX !== UINT_NULL &&
     result.cursorX <= result.x
   );
 }
@@ -512,8 +508,8 @@ function isCursorOnLeft(result) {
 function isCursorOnRight(result, x) {
   return (
     result.lineNo === result.cursorLine &&
-    result.cursorX !== SENTINEL_NULL &&
-    x !== SENTINEL_NULL &&
+    result.cursorX !== UINT_NULL &&
+    x !== UINT_NULL &&
     result.cursorX > x
   );
 }
@@ -524,7 +520,7 @@ function isCursorInComment(result) {
 
 function handleCursorDelta(result) {
   var hasCursorDelta = (
-    result.cursorDx !== SENTINEL_NULL &&
+    result.cursorDx !== UINT_NULL &&
     result.cursorLine === result.lineNo &&
     result.cursorX === result.x
   );
@@ -543,7 +539,7 @@ function resetParenTrail(result, lineNo, x) {
   result.parenTrail.startX = x;
   result.parenTrail.endX = x;
   result.parenTrail.openers = [];
-  result.maxIndent = SENTINEL_NULL;
+  result.maxIndent = UINT_NULL;
 }
 
 // INDENT MODE: allow the cursor to clamp the paren trail
@@ -595,17 +591,30 @@ function correctParenTrail(result, indentX) {
   var parens = "";
 
   while (result.parenStack.length > 0) {
-    var opener = peek(result.parenStack);
-    if (opener.x - opener.indentDelta >= indentX) {
-      result.parenStack.pop();
-      parens += MATCH_PAREN[opener.ch];
-    }
-    else {
+    var opener = peek(result.parenStack, 1);
+    var currOutside = (opener.x < indentX);
+    var prevOutside = (opener.x - opener.indentDelta < indentX);
+
+    if (prevOutside) {
+      // If an open-paren WAS outside, its `indentDelta` will be used to KEEP IT
+      // outside, by adjusting the indentation of its child lines.
       break;
     }
+    if (currOutside) {
+      // If an open-paren was JUST pushed outside and its parent open-paren was
+      // not pushed by same amount, new child line(s) will be adopted.
+      // Clear `indentDelta` since it is reserved for previous child lines only.
+      var nextOpener = peek(result.parenStack, 2);
+      if (!nextOpener || nextOpener.indentDelta !== opener.indentDelta) {
+        opener.indentDelta = 0;
+        break;
+      }
+    }
+    result.parenStack.pop();
+    parens += MATCH_PAREN[opener.ch];
   }
 
-  if (result.parenTrail.lineNo !== SENTINEL_NULL) {
+  if (result.parenTrail.lineNo !== UINT_NULL) {
     replaceWithinLine(result, result.parenTrail.lineNo, result.parenTrail.startX, result.parenTrail.endX, parens);
   }
 }
@@ -651,9 +660,9 @@ function appendParenTrail(result) {
 
 function invalidateParenTrail(result) {
   result.parenTrail = {
-    lineNo: SENTINEL_NULL,
-    startX: SENTINEL_NULL,
-    endX: SENTINEL_NULL,
+    lineNo: UINT_NULL,
+    startX: UINT_NULL,
+    endX: UINT_NULL,
     openers: []
   };
 }
@@ -699,8 +708,8 @@ function correctIndent(result) {
   var minIndent = 0;
   var maxIndent = result.maxIndent;
 
-  var opener = peek(result.parenStack);
-  if (opener !== SENTINEL_NULL) {
+  var opener = peek(result.parenStack, 1);
+  if (opener) {
     minIndent = opener.x + 1;
     newIndent += opener.indentDelta;
   }
@@ -722,8 +731,8 @@ function onIndent(result) {
   if (result.mode === INDENT_MODE) {
     correctParenTrail(result, result.x);
 
-    var opener = peek(result.parenStack);
-    var delta = (opener !== SENTINEL_NULL ? opener.indentDelta : 0);
+    var opener = peek(result.parenStack, 1);
+    var delta = (opener ? opener.indentDelta : 0);
     if (delta !== 0) {
       changeIndent(result, delta);
     }
@@ -1106,8 +1115,8 @@ function test_tabStopLine(tabStops) {
 function test_format(result, printOptions) {
   var lines = result.text.split(LINE_ENDING_REGEX);
   var hasCursor = (
-    result.cursorX !== SENTINEL_NULL &&
-    result.cursorLine !== SENTINEL_NULL &&
+    result.cursorX !== UINT_NULL &&
+    result.cursorLine !== UINT_NULL &&
 
     // could be false if `partialResult` is true and parinfer failed before reaching cursor line
     result.cursorLine < lines.length
@@ -1185,7 +1194,7 @@ function testInput(text) {
 }
 
 var API = {
-  version: "2.5.0",
+  version: "2.5.1",
   indentMode: indentMode,
   parenMode: parenMode,
   testIndentMode: testIndentMode,
